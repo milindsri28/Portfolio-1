08fc7ea58cd7f530fa49879edd7f41fc3d0fd547
Milind <120738859+milindsri28@users.noreply.github.com>
2025-05-23 01:34:48 +0530

    Fix Tailwind configuration syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-//module.exports = {
-  //content: ["./index.html"],
-  //theme: {
-    //extend: {
-      //transitionTimingFunction: {
-       // jump: "cubic-bezier(0.68, -0.55, 0.265, 1.55)",
-     // },
-   // },
-  //},
-  //plugins: [],
-  // tailwind.config.js
 module.exports = {
+  content: ["./index.html"],
   theme: {
     extend: {
       animation: {
         fadeIn: 'fadeIn 1s ease-out', // Adds a fade-in animation
       },
-      
       keyframes: {
         fadeIn: {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
         },
-      }
+      },
       transitionTimingFunction: {
-        jump: "cubic-bezier(0.68, -0.55, 0.265, 1.55)",,
-    },
-
+        jump: "cubic-bezier(0.68, -0.55, 0.265, 1.55)"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
